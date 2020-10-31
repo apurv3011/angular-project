64d5c388170ca2f20b5c10b9194cdff74de931f0
apurv121.ranjan@gmail.com <apurv121.ranjan@gmail.com>
2020-10-31 18:58:15 +0530

    first commit

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+    id: string;
+    name: string;
+    image: string;
+    featured: boolean;
+    label: string;
+    price: string;
+    category: string;
+    description: string;
+}
